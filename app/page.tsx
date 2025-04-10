0cd0707de29a318a58d348fef3e68d2397d8562f
Shrike0p <prakharsingh2208@gmail.com>
2025-04-11 04:03:40 +0530

    fix: build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AboutMe from "@/components/ui/home/about-me";
 import EarthSparkles from "@/components/ui/home/earth-sparkles";
 import HelloParticles from "@/components/ui/home/hello-section";
-import ContactMe from "@/components/ui/home/lets-connect";
 import MySkills from "@/components/ui/home/my-skills";
 import ProjectsSection from "@/components/ui/home/project/project-section";
 
